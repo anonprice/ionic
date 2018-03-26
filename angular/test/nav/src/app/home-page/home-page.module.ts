c109707274bc6ad12adfd471013a94ab016b9a54
Adam Bradley <adambradley25@gmail.com>
2018-03-26 14:30:39 -0500

    wip

--- angular/test/nav/src/app/home-page/home-page.module.ts
+++ angular/test/nav/src/app/home-page/home-page.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomePageComponent } from './home-page.component';
 import { HomePageRoutingModule } from './home-page-routing.module';
+import { IonicAngularModule } from '@ionic/angular';
 
 @NgModule({
   imports: [
     CommonModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    IonicAngularModule
   ],
   declarations: [HomePageComponent]
 })
